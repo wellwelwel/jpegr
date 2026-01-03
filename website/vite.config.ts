012526c91a585fdbf2d06a15b27c3df7937c2951
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2026-01-03 02:11:23 -0300

    chore: improve website

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,13 +1,31 @@
+import legacy from '@vitejs/plugin-legacy';
 import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: './jpegr',
+  css: {
+    postcss: {
+      plugins: [autoprefixer, cssnano],
+    },
+  },
+  plugins: [
+    react(),
+    legacy({
+      targets: ['> 0%'],
+      polyfills: true,
+    }),
+  ],
   server: {
     allowedHosts: true,
   },
   preview: {
     allowedHosts: true,
+  },
+  optimizeDeps: {
+    force: true,
   },
 });
