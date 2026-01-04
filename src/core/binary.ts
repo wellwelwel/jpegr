248df7fdd8d737f7e24117e714f39892d8e13978
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2026-01-04 09:51:32 -0300

    refactor: TOC

--- src/core/binary.ts
+++ src/core/binary.ts
@@ -4,18 +4,20 @@ export const arrayBufferToBinary = (buffer: ArrayBuffer): string => {
   if (supports.Uint8Array) {
     const bytes = new Uint8Array(buffer);
     const chars: string[] = [];
-    for (let i = 0; i < bytes.length; i++) {
+
+    for (let i = 0; i < bytes.length; i++)
       chars.push(String.fromCharCode(bytes[i]));
-    }
+
     return chars.join('');
   }
 
   if (supports.DataView) {
     const view = new DataView(buffer);
     const chars: string[] = [];
-    for (let i = 0; i < buffer.byteLength; i++) {
+
+    for (let i = 0; i < buffer.byteLength; i++)
       chars.push(String.fromCharCode(view.getUint8(i)));
-    }
+
     return chars.join('');
   }
 
@@ -27,18 +29,18 @@ export const binaryToArrayBuffer = (binary: string): ArrayBuffer => {
 
   if (supports.Uint8Array) {
     const bytes = new Uint8Array(length);
-    for (let i = 0; i < length; i++) {
-      bytes[i] = binary.charCodeAt(i);
-    }
+
+    for (let i = 0; i < length; i++) bytes[i] = binary.charCodeAt(i);
+
     return bytes.buffer;
   }
 
   if (supports.DataView) {
     const buffer = new ArrayBuffer(length);
     const view = new DataView(buffer);
-    for (let i = 0; i < length; i++) {
-      view.setUint8(i, binary.charCodeAt(i));
-    }
+
+    for (let i = 0; i < length; i++) view.setUint8(i, binary.charCodeAt(i));
+
     return buffer;
   }
 
